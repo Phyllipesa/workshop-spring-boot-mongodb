40770f02195cdfb6fd392789dba12b62292d1546
Phyllipe <phylipe.sjc@hotmail.com>
2023-11-20 16:22:44 -0300

    Simple query with query methods

--- src/main/java/com/phyllipesa/workshopmongo/domain/services/PostService.java
+++ src/main/java/com/phyllipesa/workshopmongo/domain/services/PostService.java
@@ -6,6 +6,7 @@ import com.phyllipesa.workshopmongo.domain.services.exception.ObjectNotFoundExce
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,5 +21,9 @@ public class PostService {
       throw new ObjectNotFoundException("Objeto não encontrado");
     }
     return post.get();
+  }
+
+  public List<Post> findByTitle(String text) {
+    return postRepository.findByTitleContainingIgnoreCase(text);
   }
 }
