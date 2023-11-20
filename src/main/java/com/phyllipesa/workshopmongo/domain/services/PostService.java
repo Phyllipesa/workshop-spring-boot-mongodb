ba18746c0fc1536e74f062bc5629c68d3a811481
Phyllipe <phylipe.sjc@hotmail.com>
2023-11-20 15:45:31 -0300

    Getting a post by id

---
+++ src/main/java/com/phyllipesa/workshopmongo/domain/services/PostService.java
@@ -0,0 +1,24 @@
+package com.phyllipesa.workshopmongo.domain.services;
+
+import com.phyllipesa.workshopmongo.domain.entities.Post;
+import com.phyllipesa.workshopmongo.domain.repository.PostRepository;
+import com.phyllipesa.workshopmongo.domain.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+  @Autowired
+  private PostRepository postRepository;
+
+  public Post findById(String id) {
+    Optional<Post> post = postRepository.findById(id);
+
+    if (post.isEmpty()) {
+      throw new ObjectNotFoundException("Objeto não encontrado");
+    }
+    return post.get();
+  }
+}
